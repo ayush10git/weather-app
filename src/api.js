203b15b2b844eb32c465f2a697e8d232c83fa258
Ayush Gupta <ayushgupta.ag264@gmail.com>
2024-01-21 02:53:39 +0530

    weather app

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'c653e54f2dmshdb2c875e6dd6129p1f9e6ejsnca6a9ea74e78',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "1d0f8f12fe12ebdedf2f60943d2f341d";
